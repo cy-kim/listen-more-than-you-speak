19839b01d54693d44d6360c0d72a902a8d12ff2e
cy-kim <kimseongyeun@gmail.com>
2023-02-07 15:47:39 -0500

    refactor: add simpler volume measuring fn

--- public/js/index.js
+++ public/js/index.js
@@ -29,89 +29,106 @@ window.addEventListener("load", function () {
       const audioContext = new (window.AudioContext ||
         window.webkitAudioContext)();
 
+      // Creating audio meter
       const mediaStreamSource =
         audioContext.createMediaStreamSource(audioStream);
-      meter = createAudioMeter(audioContext);
+      const meter = audioContext.createAnalyser();
       mediaStreamSource.connect(meter);
+
+      setInterval(function () {
+        const volume = calculateVolume(meter);
+        console.log("Volume:", volume);
+      }, 200);
 
       // Now setup socket
       setupSocket();
     })
     .catch(function (err) {
-      /* Handle the error */
       alert(err);
     });
-  //draw();
 });
 
-function createAudioMeter(audioContext, clipLevel, averaging, clipLag) {
-  const processor = audioContext.createScriptProcessor(512);
-  processor.onaudioprocess = volumeAudioProcess;
-  processor.clipping = false;
-  processor.lastClip = 0;
-  processor.volume = 0;
-  processor.clipLevel = clipLevel || 0.98;
-  processor.averaging = averaging || 0.95;
-  processor.clipLag = clipLag || 750;
-
-  // this will have no effect, since we don't copy the input to the output,
-  // but works around a current Chrome bug.
-  processor.connect(audioContext.destination);
-
-  processor.checkClipping = function () {
-    if (!this.clipping) {
-      return false;
-    }
-    if (this.lastClip + this.clipLag < window.performance.now()) {
-      this.clipping = false;
-    }
-    return this.clipping;
-  };
-
-  processor.shutdown = function () {
-    this.disconnect();
-    this.onaudioprocess = null;
-  };
-
-  return processor;
-}
-
-function volumeAudioProcess(event) {
-  const buf = event.inputBuffer.getChannelData(0);
-  const bufLength = buf.length;
+function calculateVolume(meter) {
+  const bufferLength = meter.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+  meter.getByteTimeDomainData(dataArray);
   let sum = 0;
-  let x;
-
-  // Do a root-mean-square on the samples: sum up the squares...
-  for (var i = 0; i < bufLength; i++) {
-    x = buf[i];
-    if (Math.abs(x) >= this.clipLevel) {
-      this.clipping = true;
-      this.lastClip = window.performance.now();
-    }
-    sum += x * x;
+  for (let i = 0; i < bufferLength; i++) {
+    sum += dataArray[i] ** 2;
   }
+  const rms = Math.sqrt(sum / bufferLength);
 
-  // ... then take the square root of the sum.
-  const rms = Math.sqrt(sum / bufLength);
-
-  // Now smooth this out with the averaging factor applied
-  // to the previous sample - take the max here because we
-  // want "fast attack, slow release."
-  this.volume = Math.max(rms, this.volume * this.averaging);
-  this.mappedVolume = Math.floor(mapRange(this.volume, 0, 1, 0, 50));
-  //document.getElementById("myAudioValue").innerHTML = this.mappedVolume;
-  myVolume = this.mappedVolume;
-  if (myVolume > 3) {
-    mySpokenFor += myVolume;
-    for (let i = 0; i < simplepeers.length; i++) {
-      if (simplepeers[i].hasConnected) {
-        simplepeers[i].simplepeer.send(JSON.stringify({ myVolume: myVolume }));
-      }
-    }
-    resizeVideos();
-  }
+  return rms;
 }
+//https://github.com/cwilso/volume-meter
+// function createAudioMeter(audioContext, clipLevel, averaging, clipLag) {
+//   const processor = audioContext.createScriptProcessor(512);
+//   processor.onaudioprocess = volumeAudioProcess;
+//   processor.clipping = false;
+//   processor.lastClip = 0;
+//   processor.volume = 0;
+//   processor.clipLevel = clipLevel || 0.98;
+//   processor.averaging = averaging || 0.95;
+//   processor.clipLag = clipLag || 750;
+
+//   // this will have no effect, since we don't copy the input to the output,
+//   // but works around a current Chrome bug.
+//   processor.connect(audioContext.destination);
+
+//   processor.checkClipping = function () {
+//     if (!this.clipping) {
+//       return false;
+//     }
+//     if (this.lastClip + this.clipLag < window.performance.now()) {
+//       this.clipping = false;
+//     }
+//     return this.clipping;
+//   };
+
+//   processor.shutdown = function () {
+//     this.disconnect();
+//     this.onaudioprocess = null;
+//   };
+
+//   return processor;
+// }
+
+// function volumeAudioProcess(event) {
+//   const buf = event.inputBuffer.getChannelData(0);
+//   const bufLength = buf.length;
+//   let sum = 0;
+//   let x;
+
+//   // Do a root-mean-square on the samples: sum up the squares...
+//   for (var i = 0; i < bufLength; i++) {
+//     x = buf[i];
+//     if (Math.abs(x) >= this.clipLevel) {
+//       this.clipping = true;
+//       this.lastClip = window.performance.now();
+//     }
+//     sum += x * x;
+//   }
+
+//   // ... then take the square root of the sum.
+//   const rms = Math.sqrt(sum / bufLength);
+
+//   // Now smooth this out with the averaging factor applied
+//   // to the previous sample - take the max here because we
+//   // want "fast attack, slow release."
+//   this.volume = Math.max(rms, this.volume * this.averaging);
+//   this.mappedVolume = Math.floor(mapRange(this.volume, 0, 1, 0, 50));
+//   //document.getElementById("myAudioValue").innerHTML = this.mappedVolume;
+//   myVolume = this.mappedVolume;
+//   if (myVolume > 3) {
+//     mySpokenFor += myVolume;
+//     for (let i = 0; i < simplepeers.length; i++) {
+//       if (simplepeers[i].hasConnected) {
+//         simplepeers[i].simplepeer.send(JSON.stringify({ myVolume: myVolume }));
+//       }
+//     }
+//     resizeVideos();
+//   }
+// }
 
 function mapRange(value, a, b, c, d) {
   // first map value from (a..b) to (0..1)
@@ -146,12 +163,13 @@ function resizeVideos() {
   }
 }
 
+/*Socket*/
 function setupSocket() {
   socket = io.connect();
 
   socket.on("connect", function () {
     console.log("**Socket Connected**");
-    console.log("My socket id: ", socket.id);
+    console.log("My socket id:   ", socket.id);
 
     // Tell the server we want a list of the other users
     socket.emit("list");
@@ -282,20 +300,6 @@ class SimplePeerWrapper {
         peerVideo.play();
       };
       this.peerVideo = peerVideo;
-
-      /*
-      audioContext = new (window.AudioContext || window.webkitAudioContext)();
-
-      playStream(this.peerStream, this.volume);
-
-      let newVolume = document.createElement("p");
-      newVolume.id = this.socket_id;
-      newVolume.zIndex = 1000;
-      document.body.appendChild(newVolume);
-
-      mediaStreamSource = audioContext.createMediaStreamSource(this.peerStream);
-      meter = this.createAudioMeter(audioContext);
-      mediaStreamSource.connect(meter);*/
     });
 
     this.simplepeer.on("data", (data) => {
